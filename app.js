bff796abc9c5492338bfb53a88119c756100e108
ahmed <ahmedlearn01@gmail.com>
2022-03-09 10:56:43 -0800

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const mongoose = requier('mongoose');
+const multer = require('multer');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
+const methodOverride = require('method-override');
 
 const app = express();
 
+// middwares
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+
+// mongo ui
+const mongoURI = process.env.MONGO_URL;
 
 app.get('/', (req,res)=>{
     res.render('index');
